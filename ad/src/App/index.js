eb329e7ec5492f94fb560f1bb38f582f18bdbc9b
Denis Rudik <rudikdenis@gmail.com>
2021-12-02 22:41:02 +0200

    null

--- ad/src/App/index.js
+++ ad/src/App/index.js
@@ -1,6 +1,7 @@
 import { Route, Routes} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Home from '../containers/Home'
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Login from '../containers/Login';
@@ -12,6 +13,8 @@ function App() {
     <>
         <Header />
         <Routes>
+
+          <Route exact path='/'  element={ <Home /> } />
           <Route path='/login'  element={ <Login /> } />
           <Route path='/registration' element={ <Registration /> } />
           <Route path='/*' element={ <span style={{color: "black"}}>404 page not found</span> } />
---
+++ ad/src/containers/Home/index.js
@@ -0,0 +1,1 @@
+function Home() {return(<>Hello World!</>);}export default Home;
